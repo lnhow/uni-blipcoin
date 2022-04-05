92d678a292c19e9cbb889a431c1396cfd98d13c4
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2022-04-05 07:48:24 +0700

    Change nodejs's crypto to crypto-js in client app
    Due to nodejs's crypto not present

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,9 +1,11 @@
 import { ec as EC } from 'elliptic';
-import crypto from 'crypto';
+import sha256 from 'crypto-js/sha256';
 
 export const ec = new EC();
 
 export const hashDataSHA256 = (data) => {
   const strData = JSON.stringify(data);
-  return crypto.createHash('sha256').update(strData).digest('hex');
+  // The same as: crypto.createHash('sha256').update(strData).digest('hex');
+  // But do not require nodejs's crypto module
+  return sha256(strData).toString();
 }
